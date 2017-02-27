415221e37c1567d8c3c7da702a169217224467ea
AndersEllinge <andersellinge.ae@gmail.com>
2017-02-27 12:11:10 +0100

    I fucked up lol

---
+++ Anders_plugin/firstPlugIn.cpp
@@ -0,0 +1,141 @@
+#include "firstPlugIn.hpp"
+#include <RobWorkStudio.hpp>
+#include <boost/bind.hpp>
+
+
+
+using rw::kinematics::State;
+using rw::models::WorkCell;
+using rws::RobWorkStudioPlugin;
+
+firstPlugIn::firstPlugIn():
+	RobWorkStudioPlugin("firstPlugInUI", QIcon(":/pa_icon.png"))
+{
+	setupUi(this);
+
+	//Connect buttons and stuff
+
+	QString sPath = "/home/osboxes/Desktop/Git/Bachelor-F17/";
+
+	dirmodel = new QFileSystemModel(this);
+	treeView->setModel(dirmodel);
+	treeView->setRootIndex(dirmodel->setRootPath(sPath));
+
+	connect(fileNameBtn, SIGNAL(pressed()), this, SLOT(btnPressed()));
+	connect(workcellBtn, SIGNAL(pressed()), this, SLOT(btnPressed()));
+	connect(loadObjectFrame, SIGNAL(pressed()), this, SLOT(btnPressed()));
+	connect(treeView, SIGNAL(clicked(const QModelIndex &)), this, SLOT(treeViewPressed(const QModelIndex &)));
+}
+
+firstPlugIn::~firstPlugIn()
+{
+}
+
+void firstPlugIn::initialize() {
+    	getRobWorkStudio()->stateChangedEvent().add(boost::bind(&firstPlugIn::stateChangedListener, this, _1), this);
+}
+
+void firstPlugIn::open(WorkCell* workcell)
+{
+	_workcell = workcell;
+}
+
+void firstPlugIn::close()
+{
+}
+
+void firstPlugIn::treeViewPressed(const QModelIndex & index)
+{
+	fName = dirmodel->fileInfo(index).absoluteFilePath();		// keep the information about the file
+}
+
+void firstPlugIn::btnPressed()
+{
+	QObject *obj = sender();
+	if(obj==workcellBtn) {
+	rw::models::WorkCell::Ptr workcell = getRobWorkStudio()->getWorkCell();
+	log().info() << "Info for scene WorkCell\n";
+	printInfo(workcell);
+	}
+
+	else if(obj==fileNameBtn) {
+	log().info() << fName.toStdString() << "\n";
+	getRobWorkStudio()->openFile(fName.toStdString()); // open whatever robwork project file with rws
+	}
+
+	else if(obj==loadObjectFrame) {
+
+	rw::models::WorkCell::Ptr workcell = getRobWorkStudio()->getWorkCell(); // Get WorkCell from scene
+
+	boost::shared_ptr<DummyWorkcell> dwc = rw::loaders::XMLRWParser::parseWorkcell(fName.toStdString());
+
+		for (int i = 0; i < dwc->_framelist.size(); i++){
+			createFrame(dwc->_framelist[i], workcell);
+		}
+	}
+}
+
+rw::kinematics::Frame* firstPlugIn::createFrame(DummyFrame dFrame, rw::models::WorkCell::Ptr wc) {
+    rw::kinematics::Frame* frame = NULL;
+    rw::kinematics::State state;
+    bool stateChanged = 0;
+
+    if (dFrame._type == "Fixed"){
+        frame = new rw::kinematics::FixedFrame(dFrame.getName(), dFrame._transform);
+    }
+
+    else if (dFrame._type == "Movable") {
+        rw::kinematics::MovableFrame* movFrame = new rw::kinematics::MovableFrame(dFrame.getName());
+        frame = movFrame;
+        wc->addFrame(frame);
+        state = getRobWorkStudio()->getWorkCell()->getDefaultState();
+        movFrame->setTransform(dFrame._transform, state);
+        stateChanged = 1;
+    }
+
+    else if (dFrame._type == "Prismatic") {
+        frame = new rw::models::PrismaticJoint(dFrame.getName(), dFrame._transform);
+    }
+
+    else if (dFrame._type == "Revolute") {
+        frame = new rw::models::RevoluteJoint(dFrame.getName(), dFrame._transform);
+    }
+
+    else if (dFrame._type == "EndEffector") {
+        frame = new rw::kinematics::FixedFrame(dFrame.getName(), dFrame._transform);
+    }
+
+    else{
+        RW_THROW("FRAME TYPE ERROR");
+    }
+
+    if (stateChanged){
+        state = getRobWorkStudio()->getWorkCell()->getStateStructure()->upgradeState(state); // Update workcell to new state
+        getRobWorkStudio()->setState(state); // Update RWS to the new state
+    }
+
+    return frame;
+}
+
+void firstPlugIn::printInfo(rw::models::WorkCell::Ptr wc) {
+    log().info() << "Found " << wc->getFrames().size() << " frames:\n"; // List frames
+    for (int i = 0; i < wc->getFrames().size(); i++) {
+        log().info() << wc->getFrames()[i]->getName() << "\n";
+    }
+    log().info() << "\n";
+
+    log().info() << "Found " << wc->getDevices().size() << " devices:\n"; // List devices
+    for (int i = 0; i < wc->getDevices().size(); i++) {
+        log().info() << wc->getDevices()[i]->getName() << "\n";
+    }
+    log().info() << "\n\n";
+}
+
+void firstPlugIn::stateChangedListener(const State& state)
+{
+}
+
+#if !RWS_USE_QT5
+#include <QtCore/qplugin.h>
+Q_EXPORT_PLUGIN(firstPlugIn);
+#endif
---
+++ Anders_plugin/firstPlugIn.hpp
@@ -0,0 +1,59 @@
+#ifndef FIRSTPLUGIN_HPP
+#define FIRSTPLUGIN_HPP
+
+#include <RobWorkStudio.hpp>
+#include <RobWorkStudioConfig.hpp>
+#include <rws/RobWorkStudioPlugin.hpp>
+#include "ui_firstPlugIn.h"
+#include <rw/common/Ptr.hpp>
+#include <QtCore>
+#include <QtGui>
+#include <rw/loaders/rwxml/XMLParserUtil.hpp>
+#include <rw/loaders/rwxml/XMLRWParser.hpp>
+#include <rw/loaders/rwxml/XMLRWLoader.hpp>
+
+#include <rw/kinematics/FixedFrame.hpp>
+#include <rw/kinematics/MovableFrame.hpp>
+#include <rw/models/RevoluteJoint.hpp>
+#include <rw/models/PrismaticJoint.hpp>
+
+namespace rw { namespace models { class Device; } }
+
+class firstPlugIn: public rws::RobWorkStudioPlugin, private Ui::firstPlugIn
+{
+
+QFileSystemModel *dirmodel;
+QFileSystemModel *filemodel;
+
+QString fName;
+
+rw::models::WorkCell* _workcell;
+
+Q_OBJECT
+Q_INTERFACES( rws::RobWorkStudioPlugin )
+#if RWS_USE_QT5
+Q_PLUGIN_METADATA(IID "dk.sdu.mip.Robwork.RobWorkStudioPlugin/0.1" FILE "plugin.json")
+#endif
+public:
+    firstPlugIn();
+	virtual ~firstPlugIn();
+
+    virtual void open(rw::models::WorkCell* workcell);
+
+    virtual void close();
+
+    virtual void initialize();
+
+private:
+    rw::kinematics::Frame* createFrame(DummyFrame dFrame, rw::models::WorkCell::Ptr wc);
+
+private slots:
+    void treeViewPressed(const QModelIndex & index);
+    void btnPressed();
+    void printInfo(rw::models::WorkCell::Ptr wc);
+    void stateChangedListener(const rw::kinematics::State& state);
+
+
+};
+
+#endif
